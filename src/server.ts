39c54d0dae7ea8a9348d0e083bce280da9fdf3d3
Samratcodebase <samratcodebase@gmail.com>
2026-03-13 22:44:51 +0530

    Boiler Plate Code

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import type { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Typescript");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
